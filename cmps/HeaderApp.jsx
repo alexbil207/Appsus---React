0585901843ee87312795379172819421a69b1af4
AlexBil <alexbil207@gmail.com>
2021-05-04 15:09:36 +0300

    menu add

--- cmps/HeaderApp.jsx
+++ cmps/HeaderApp.jsx
@@ -9,7 +9,7 @@ export class HeaderApp extends React.Component {
             <React.Fragment>
                 <div className="header-container container flex align-center">
                     <div className="logo">
-                        <h1>LOGO</h1>
+                        <h1>App/sus</h1>
                     </div>
                     <div className="nav-btn">
                         <img src="../assets/icons/menu.svg" onClick={() => {

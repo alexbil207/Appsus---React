a299dc7b359aca13003f89c78f643825e5a615f9
Alex <sp00king@gmail.com>
2021-05-04 16:26:11 +0300

    fix background

--- pages/home.jsx
+++ pages/home.jsx
@@ -5,7 +5,7 @@ export class Home extends React.Component {
 
     render() {
         return (
-            <section className="home">
+            <section className="home home-img">
                 <div className="hero container flex column align-center space-between">
                     <h2>Welcome to your digital workspace</h2>
                     <Link to="/Mail" className="btn btn-hero">Get Started</Link>

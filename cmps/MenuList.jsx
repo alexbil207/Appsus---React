1fca33f8034db76f330de2a19709d1e4d1693745
AlexBil <alexbil207@gmail.com>
2021-05-06 15:36:32 +0300

    note bug fix

--- cmps/MenuList.jsx
+++ cmps/MenuList.jsx
@@ -2,10 +2,10 @@ const { NavLink } = ReactRouterDOM;
 
 export function MenuList({ closeMenu }) {
     return (<React.Fragment>
-        <NavLink to="/About"><img src="../assets/icons/about.png" onClick={closeMenu} /></NavLink>
-        <NavLink to="/Mail"><img src="../assets/icons/mail.png" onClick={closeMenu} /></NavLink>
-        <NavLink to="/Note"><img src="../assets/icons/notes.png" onClick={closeMenu} /></NavLink >
-        <NavLink to="/Book"><img src="../assets/icons/book.png" onClick={closeMenu} /></NavLink >
+        <NavLink to="/About"><img src="./assets/icons/about.png" onClick={closeMenu} /></NavLink>
+        <NavLink to="/Mail"><img src="./assets/icons/mail.png" onClick={closeMenu} /></NavLink>
+        <NavLink to="/Note"><img src="./assets/icons/notes.png" onClick={closeMenu} /></NavLink >
+        <NavLink to="/Book"><img src="./assets/icons/book.png" onClick={closeMenu} /></NavLink >
     </React.Fragment>
     )
 }
--- pages/About.jsx
+++ pages/About.jsx
@@ -2,7 +2,7 @@ export function About() {
     return (
         <section className="about container flex gap space-evenly ">
             <div className="img-container flex column space-between align-center">
-                <img src="../assets/img/alex_bil.jpg" alt="" />
+                <img src="./assets/img/alex_bil.jpg" alt="" />
                 <h2>Alex Bilinski</h2>
                 <h3>Head of Mail Production Development</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sequi repellat qui eos magnam itaque repellendus nostrum necessitatibus recusandae minus, aperiam odio, inventore totam autem aliquam consequatur consequuntur fuga maxime.
@@ -10,7 +10,7 @@ export function About() {
                 Earum soluta ipsam a, id alias magnam architecto obcaecati ipsum totam aut in, reiciendis excepturi asperiores eos laudantium officiis maxime consectetur, unde tempora facere recusandae ea vero similique! Libero, accusamus.</p>
             </div>
             <div className="img-container flex column space-between align-center">
-                <img src="../assets/img/alex_gordon.jpg" alt="" />
+                <img src="./assets/img/alex_gordon.jpg" alt="" />
                 <h2>Alex Gordon</h2>
                 <h3>Head of Note Production Development</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sequi repellat qui eos magnam itaque repellendus nostrum necessitatibus recusandae minus, aperiam odio, inventore totam autem aliquam consequatur consequuntur fuga maxime.

420374d34c52708b64d6e273e609a53a6b8f13c9
Alex <sp00king@gmail.com>
2021-05-06 11:58:01 +0300

    note app adding notes

--- apps/Note/cmps/NoteController.jsx
+++ apps/Note/cmps/NoteController.jsx
@@ -1,10 +1,11 @@
+import { noteService } from '../services/note.service.js'
+
 export class NoteController extends React.Component {
     state = {
         type: 'text',
         txt: 'Enter Your Text',
     }
     changeInputText = (ev) => {
-        console.log(ev.target.name)
         const inputType = ev.target.name;
         switch (inputType) {
             case 'text':
@@ -22,17 +23,47 @@ export class NoteController extends React.Component {
         }
     }
 
+    handleChange = ({ target }) => {
+        const { txt } = this.state;
+        const field = target.name
+        const value = target.value
+        console.log(target.value);
+        this.setState(prevState => ({ ...prevState, [field]: [value] }))
+        this.setState({ txt: target.value })
+    }
+
+    loadNotes = () => {
+        noteService.query().then(notes => {
+            this.setState(prevState => ({ ...prevState, notes }))
+        })
+        console.log('loading notes');
+    }
+
+    addNote = () => {
+        const { txt } = this.state;
+        const { type } = this.state;
+        { type === txt && <TextNote note={txt} /> }
+        noteService.addNote(txt).then(() => this.loadNotes())
+
+
+        // { type === 'photo' && <PhotoNote note={txt} /> }
+        // { type === 'video' && <VideoNote note={txt} /> }
+
+        //this.setState({ txt: '' })
+        //after change state of txt to '' need to update the input target.value to '' too
+    }
 
     render() {
         const { txt } = this.state;
+        const { type } = this.state;
         return (
             <div className="add-note-container flex space-evenly align-center">
-                <textarea type="search" placeholder={txt}></textarea>
+                <textarea type="search" onChange={this.handleChange} placeholder={txt}></textarea>
                 <button name="text" onClick={this.changeInputText}><i className="fas fa-font fa-2x"></i></button>
                 <button name="video" onClick={this.changeInputText}><i className="fab fa-youtube fa-2x"></i></button>
                 <button name="photo" onClick={this.changeInputText}><i className="fas fa-image fa-2x"></i></button>
                 <button name="todo" onClick={this.changeInputText}><i className="fas fa-tasks fa-2x"></i></button>
-                <button><i className="fas fa-plus fa-2x"></i></button>
+                <button className="add-btn" name="add" onClick={this.addNote}><i className="fas fa-plus fa-2x"></i></button>
             </div>
         )
     }
--- apps/Note/cmps/NotePreview.jsx
+++ apps/Note/cmps/NotePreview.jsx
@@ -16,23 +16,22 @@ export class NotePreview extends React.Component {
     }
 
 
-
-
     render() {
         const { note, type } = this.state;
         const { removeNote } = this.props;
-        console.log('type', type);
+        //console.log('type', type);
         if (!note) return <h1>Loading...</h1>
-        console.log(note);
+        //console.log(note);
 
         return (
-
             <article id={note.id} className='note-preview'>
                 {type === 'text' && <TextNote note={note} />}
                 {type === 'photo' && <PhotoNote note={note} />}
                 {type === 'video' && <VideoNote note={note} />}
                 <div className="preview-btns flex space-between">
                     <button onClick={() => removeNote(note)}><i className="fas fa-trash-alt fa-2x"></i></button>
+                    <button ><i className="fas fa-palette fa-2x"></i></button>
+                    <button ><i className="fas fa-edit fa-2x"></i></button>
                     <button ><i className="fas fa-thumbtack fa-2x"></i></button>
                 </div>
             </article>

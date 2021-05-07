33f94632d1495e91610cbea84e898a659569444b
AlexBil <alexbil207@gmail.com>
2021-05-07 20:18:38 +0300

    mail images

--- apps/Mail/cmps/NewMail.jsx
+++ apps/Mail/cmps/NewMail.jsx
@@ -1,11 +1,12 @@
 export class NewMail extends React.Component {
     state = {
-        from: null,
+        from: 'alex207@gmail.com',
         to: null,
         body: null,
         subject: null,
         isSend: true,
         isRead: true,
+        url: null,
     }
     handleChanges = (ev) => {
         const field = ev.target.name;
@@ -27,7 +28,8 @@ export class NewMail extends React.Component {
         return (
             <form className="new-mail-container container flex column">
                 <label htmlFor="to">To: <input name="to" id="to" type="email" onChange={this.handleChanges} required></input></label>
-                <label htmlFor="from">From: <input name="from" id="from" type="email" onChange={this.handleChanges} required></input></label>
+                <label htmlFor="from">From: <input name="from" id="from" type="email" value={this.state.from}></input></label>
+                <label htmlFor="url">Image URL: <input name="url" id="url" type="url" onChange={this.handleChanges}></input></label>
                 <label htmlFor="subject">Subject: <input name="subject" id="subject" type="text" onChange={this.handleChanges} required></input></label>
                 <textarea name="body" placeholder="Body" onChange={this.handleChanges} required></textarea>
                 <div className="btns flex" >
